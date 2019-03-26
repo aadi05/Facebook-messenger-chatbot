98c15482bb6578c2cf06a38ca0ff58e73afd87c9
Aditya Bhogate <adityabhogate5@gmail.com>
2019-03-26 22:24:58 +0530

    'first'

--- server.py
+++ server.py
@@ -131,4 +131,6 @@ def getFact():
     return jsonify({"fulfillmentText":output_string})
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
